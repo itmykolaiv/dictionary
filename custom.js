b20ce5754918d693edaae978844a4228b31cfa64
igor <igor.payki@gmail.com>
2017-11-04 18:01:32 +0200

    Dictionary.html custom.js custom.js script.js

--- custom.js
+++ custom.js
@@ -1,17 +1,15 @@
 //Send request
-function send_request(command) {
-    var myHeaders = new Headers();
-    myHeaders.append("X-Mashape-Key", "zd1IXUWYTnmshVbob13BKiSwWEKdp1PfBb3jsnC4LZHFgUehE1");
-    fetch('http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/dictionary/' + command, {headers: myHeaders})
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(data) {
-        console.log(data);
-      })
-      .catch( function() {
-        console.log("Somthing wrong!!!");
-      });
-  }
-  
+function send_request(command, query, cb) {
+  var baseUrl = 'http://ec2-52-14-195-100.us-east-2.compute.amazonaws.com/dictionary';
+  var myHeaders = new Headers();
+  myHeaders.append("X-Mashape-Key", "zd1IXUWYTnmshVbob13BKiSwWEKdp1PfBb3jsnC4LZHFgUehE1");
 
+  fetch(baseUrl + '/' + command + '/?' + query, {headers: myHeaders})
+      .then(function(response) {
+          return response.json();
+      })
+      .then(cb)
+      .catch( function() {
+          console.log("Somthing wrong!!!");
+      });
+}
--- script.js
+++ script.js
@@ -10,4 +10,23 @@ document.addEventListener("DOMContentLoaded", function() {
             $alpha.appendChild($button);
         }
     }
+    var search = document.querySelector('.js_search-btn');
+    search.addEventListener('click', function() {
+        var input = document.querySelector('.js_text-input');
+        var word = input.value;
+        send_request(word + '/definitions', '', function(data) {
+            var expl = document.querySelector('#explanation');
+            var word_div = document.createElement('div');
+            word_div.innerHTML = data.word + ':';
+            expl.append(word_div);
+            var div_defs = document.createElement('div');
+            for (var i = 0; i < data.definitions.length; i++) {
+                var div_one_def = document.createElement('div');
+                div_one_def.className = 'word-definition';
+                div_one_def.innerHTML = data.definitions[i].definition;
+                expl.append(div_one_def);
+            }
+        });
+        
+    });
 });
